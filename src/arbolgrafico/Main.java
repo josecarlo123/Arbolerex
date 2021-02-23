402a4e4dbcb3cfb6fdf4b328235375e906cc07c3
Jose Carlos <jose.carlos.j3c45.jc@gmail.com>
2021-02-22 22:14:59 -0600

    PRUEBA 2

--- src/arbolgrafico/Main.java
+++ src/arbolgrafico/Main.java
@@ -1,6 +1,10 @@
 
 package arbolgrafico;
 
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.ArrayList;
+
 /**
  *
  * @author Jose Carlos
@@ -14,3 +18,30 @@ public class Main {
         new Gui().setVisible(true);
     }
 }
+/*
+
+
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        if(!arbolGeneral.estaVacia())
+        {
+            dibujar(g,arbolGeneral.getRaiz(),getWidth()/2,30,getWidth()/5);
+        }
+    }
+
+    private void dibujar(Graphics g, Elemento<E> raiz, int x, int y, int espacioH)
+    {
+        g.drawOval(x-radio,y-radio,2*radio,2*radio);
+        g.drawString(raiz.getElemento() + "", x - 6, y + 4);
+        ArrayList<Elemento<E>>hijos=arbolGeneral.obtenerHijos(raiz);
+        coordenadas.put(raiz,new Point(x,y));
+        for(Elemento<E> hijo:hijos)
+        {
+            Point punto=coordenadas.get(arbolGeneral.obtenerPadre(hijo));
+            dibujarLinea(g,x-espacioH,y+espacioVertical,punto.x,punto.y);
+            dibujar(g,hijo,x-espacioH,y+espacioVertical,espacioH/hijos.size());
+            x+=espacioH;
+        }
+    }
+*/
